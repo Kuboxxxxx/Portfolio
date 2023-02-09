572e55593f8dd94f7fb1799d48bc365622f8a83c
unknown <jakubkarols@gmail.com>
2023-02-09 22:34:40 +0000

    Fixed some spelling issues. Made project cards fully clickable anywhere. Added external links to socials to contact section

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+// Dark mode
+
 const darkModeToggleButton = document.querySelector('#darkModeToggleButton')
 const darkModeValue = JSON.parse(localStorage.getItem("darkMode"))
-
 
 const darkModeToggle = () => {
     document.body.classList.toggle("dark")
@@ -12,8 +13,37 @@ const darkModeToggle = () => {
     }
 }
 
-if (darkModeValue){
+if (darkModeValue.status){
     document.body.classList.add('dark')
 }
 
 darkModeToggleButton.addEventListener("click", darkModeToggle)
+
+//Clickable projects
+
+const bambooProject = document.querySelector("#bambooLink")
+const restoProject = document.querySelector("#restoLink")
+const mafiaProject = document.querySelector("#mafiaLink")
+const wtrProject = document.querySelector("#wtrLink")
+
+const bambooLink = "https://kuboxxxxx.github.io/ClickerGame/"
+const restoLink = "https://github.com/Kuboxxxxx/Resto"
+const mafiaLink = "https://pusheeneiro.itch.io/black-market"
+const wtrLink = "https://pusheeneiro.itch.io/wtr"
+
+const openWindow = (link) => {
+    window.open(link)
+}
+
+bambooProject.addEventListener("click", function(){
+    window.open(bambooLink)
+})
+restoProject.addEventListener("click", function(){
+    openWindow(restoLink)
+})
+mafiaProject.addEventListener("click", function(){
+    openWindow(mafiaLink)
+})
+wtrProject.addEventListener("click", function(){
+    openWindow(wtrLink)
+})
